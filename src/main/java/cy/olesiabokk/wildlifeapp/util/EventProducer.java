d181d65d5af335481f5d973034a191c246383252
OlesiaBokk <orbokk@gmail.com>
2024-02-22 13:02:13 +0200

    Refactor methods, increased chance for attack methods, fixed typos.

--- src/main/java/cy/olesiabokk/wildlifeapp/util/EventProducer.java
+++ src/main/java/cy/olesiabokk/wildlifeapp/util/EventProducer.java
@@ -42,7 +42,7 @@ public class EventProducer {
             }
             Thread.sleep(200);
         }
-        System.out.println("Manul had a good and interesting life. Now he's going to Manuls' Heaven");
+        System.out.println("Manul had a good and interesting life. Now it goes to Manuls' Heaven.");
     }
 
     private int getResult() {
@@ -73,32 +73,23 @@ public class EventProducer {
     }
 
     private void sleep(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy + 20;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() + 20);
         checkEnergy(manul);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel());
         checkHealth(manul);
         System.out.println("Manul slept and feels better. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void run(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 5;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 5);
         checkEnergy(manul);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel());
         checkHealth(manul);
         System.out.println("Manul run and lost 5 energy points. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void eatMouse(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 7;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 7);
         checkEnergy(manul);
         manul.setHealthLevel(manul.getHealthLevel() + (int) (manul.getJawCoeff() * 3));
         checkHealth(manul);
@@ -106,58 +97,40 @@ public class EventProducer {
     }
 
     private void eatHare(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 10;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 10);
         checkEnergy(manul);
-        health = health + (int) (manul.getJawCoeff() * 5);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel() + (int) (manul.getJawCoeff() * 5));
         checkHealth(manul);
         System.out.println("Wow! Manul caught a hare and recovered health. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void eatMarmot(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 13;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 13);
         checkEnergy(manul);
-        health = health + (int) (manul.getJawCoeff() * 8);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel() + (int) (manul.getJawCoeff() * 8));
         checkHealth(manul);
         System.out.println("Fantastic! Manul caught a marmot and recovered health. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void playNewFriend(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 8;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 8);
         checkEnergy(manul);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel());
         checkHealth(manul);
         System.out.println("Manul met new friend and they played together. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void defendTerritory(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 13;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 13);
         checkEnergy(manul);
-        health = health - 3;
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel() - 3);
         checkHealth(manul);
         System.out.println("Someone wanted to kick manul away from its home, but our hero won. Manul was a bit bitten and tired.\n" +
                 "Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void attackedByFox(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 7;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 7);
         checkEnergy(manul);
         manul.setHealthLevel(manul.getHealthLevel() - 10);
         checkHealth(manul);
@@ -165,10 +138,7 @@ public class EventProducer {
     }
 
     private void attackedByDog(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 10;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 10);
         checkEnergy(manul);
         manul.setHealthLevel(manul.getHealthLevel() - 15);
         checkHealth(manul);
@@ -176,26 +146,19 @@ public class EventProducer {
     }
 
     private void findShelter(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 2;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 2);
         checkEnergy(manul);
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel());
         checkHealth(manul);
-        System.out.println("Manul found s shelter, now it's safe. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
+        System.out.println("Manul found a shelter, now it's safe. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     private void groomFur(Manul manul) {
-        int energy = manul.getEnergy();
-        int health = manul.getHealthLevel();
-        energy = energy - 1;
-        manul.setEnergy(energy);
+        manul.setEnergy(manul.getEnergy() - 1);
         checkEnergy(manul);
-        health = health + 1;
-        manul.setHealthLevel(health);
+        manul.setHealthLevel(manul.getHealthLevel() + 1);
         checkHealth(manul);
-        System.out.println("Manul groomed itself and recovered some energy and health. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
+        System.out.println("Manul groomed itself and recovered some health. Current energy = " + manul.getEnergy() + " Current health = " + manul.getHealthLevel());
     }
 
     public void checkEnergy(Manul manul) {
@@ -210,26 +173,20 @@ public class EventProducer {
     }
 
     public void checkHealth(Manul manul) {
-        if (manul.getHealthLevel() > 100) {
-            manul.setHealthLevel(100);
-        } else if (manul.getHealthLevel() < 100 && manul.getHealthLevel() > 0) {
-            manul.setHealthLevel(manul.getHealthLevel());
-        } else if (manul.getHealthLevel() <= 0) {
+        if (manul.getHealthLevel() <= 0) {
             manul.setHealthLevel(0);
+        } else if (manul.getHealthLevel() > 100) {
+            manul.setHealthLevel(100);
+        } else {
+            manul.setHealthLevel(manul.getHealthLevel());
         }
     }
 
     public void getHitPoint(Manul manul) {
-        if (manul.getEnergy() <= 0) {
-            manul.setHealthLevel(manul.getHealthLevel() - 3);
-        }
+        manul.setHealthLevel(manul.getHealthLevel() - 3);
     }
 
     public boolean isAlive(Manul manul) {
-        if (manul.getHealthLevel() <= 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return manul.getHealthLevel() > 0;
     }
 }
